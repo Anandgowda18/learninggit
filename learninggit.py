80eab51e3c37e34a433191133eb565403c19adb9
Anandgowda18 <32433566+Anandgowda18@users.noreply.github.com>
2019-04-30 11:10:14 +0530

    I am creating a branch

--- learninggit.py
+++ learninggit.py
@@ -1,2 +1,4 @@
 print("Heloo world")
 print("I am updating today")
+print("I am creating a new branch")
+print("to create a new branch is to have some configuration or code changes which needn't be put into the main(master) branch(repository")
